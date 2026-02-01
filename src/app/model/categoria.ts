f60bb8dd1d4e3e014162a5c5f8473d9ec684c723
alvaro <alvaroch975@gmail.com>
2026-02-01 21:10:39 +0100

    arreglo model + servicev2

--- src/app/model/categoria.ts
+++ src/app/model/categoria.ts
@@ -11,19 +11,6 @@ export interface ICategoria {
 export interface ITemporada {
   id: number
   descripcion: string
-  club: IClub
-  categorias: number
 }
 
-export interface IClub {
-  id: number
-  nombre: string
-  direccion: string
-  telefono: string
-  fechaAlta: string
-  imagen: any
-  temporadas: number
-  noticias: number
-  tipoarticulos: number
-  usuarios: number
-}
+
